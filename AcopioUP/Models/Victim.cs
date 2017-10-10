bc9833aab71ad03db782c1a1ace3c7e25c834a47
CesarBerard <cesar.berard@hotmail.com>
2017-10-10 13:52:15 -0500

    Added Victim Model

---
+++ AcopioUP/Models/Victim.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AcopioUP.Models
+{
+    public class Victim
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Email { get; set; }
+    }
+}
