bd7043ebab458ec5b0afdf4cac4099252f7f1533
irving-as <irving.as.123@gmail.com>
2017-10-10 10:42:43 -0500

    Add Product domain Model. Create DBSet for products. Add new migration to database.

---
+++ AcopioUP/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcopioUP.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImgSrc { get; set; }
+    }
+}
