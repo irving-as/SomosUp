9f864fddd00926b938991641c511ca383ebbc9e5
irving-as <irving.as.123@gmail.com>
2017-10-11 23:56:21 -0500

    Add CanManageProducts and CanManageVictims user roles. Seed admin user. Limit all controllers to signed in user (except for a few exceptions on HomeController, etc). Create readonly version of the Products List view.

---
+++ AcopioUP/Models/RoleNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcopioUP.Models
+{
+    public static class RoleNames
+    {
+        public const string CanManageProducts = "CanManageProducts";
+        public const string CanManageVictims = "CanManageVictims";
+    }
+}
